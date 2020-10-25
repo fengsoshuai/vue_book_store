3cc44b72e51d91e9f5b5bcdfd4e3cc0bcaeb7bf8
fengsoshuai <fengsoshuai@163.com>
2020-10-25 18:03:13 +0800

    home页面的布局

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, Container, Header, Aside, Main, Input, Menu, Submenu, MenuItemGroup, MenuItem } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Aside)
+Vue.use(Input)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <el-container style="height: 100%">
+      <!-- 头部区域 -->
+      <el-header>
+        <div>
+          <img class="logo" src="../assets/logo.png" alt="后台管理系统">
+          <span>后台管理系统</span>
+        </div>
+        <el-button type="info" @click="logout">退出</el-button>
+      </el-header>
+      <!-- 页面主体区域 -->
+      <el-container>
+        <!-- 左侧区域 -->
+        <el-aside width="200px">
+          <el-menu
+            background-color="dodgerblue"
+            text-color="#fff"
+            active-text-color="#ccc">
+            <!--1级菜单-->
+            <el-submenu index="1">
+              <!--1级菜单的模板-->
+                <template slot="title">
+                  <i class="el-icon-menu"></i>系统管理
+                </template>
+                <!--2级菜单-->
+                <el-menu-item index="1-1-1">
+                  <template slot="title"><i class="el-icon-service"></i>
+                    <span>用户管理</span>
+                  </template>
+                </el-menu-item>
+              </el-submenu>
+          </el-menu>
+        </el-aside>
+        <!-- 右侧内容主体 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  created () {
+    this.getMenuList()
+  },
+  methods: {
+    logout () {
+      alert('退出')
+    },
+    getMenuList () {
+
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+  .logo {
+    width: 40px;
+  }
+  .el-header{
+    background-color: dodgerblue;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+      display: flex;
+      align-items: center;
+      span{
+        margin-left: 15px;
+      }
+    }
+  }
+  .el-aside{
+    background-color: dodgerblue;
+  }
+  .el-main{
+    background-color: antiquewhite;
+  }
+</style>
